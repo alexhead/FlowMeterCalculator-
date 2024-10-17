64cc7b65ea5adcdef142aed1843dc9550cc507c1
Aleksei Ivanov <alexhead4@gmail.com>
2024-10-18 00:04:10 +0500

    __init__: configure simple react project with webpack

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# FlowMeterCalculator-
+# FlowMeterCalculator
+
 Special calculator app for precise water meter configuration, designed to the setup process is easier.
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+/**
+ * This is the main component of the application.
+ * @returns {JSX.Element}
+ */
+const App = () => {
+  return <div>Flow Meter Calculator</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
