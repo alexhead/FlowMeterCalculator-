c16a28877eeffd51fb882d4c92fe247c7c8432e2
Urzael59 <andry137.eremeev@gmail.com>
2024-10-28 16:07:18 +0500

    Finished logo and footer on auto

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import * as styles from './Header.module.css';
-import logo from './../../public/images/menuButton.png';
+import button from './../../public/images/menuButton.png';
+import title from './../../public/images/Title.png';
+import logo from './../../public/images/Logo.png';
 
 const Header = () => {
   return (
     <div className={styles.header}>
-      <div className={styles.header__logo}>Logo</div>
-      <div className={styles.header__title}>FlowMeterCalc</div>
-      <div onClick={() => alert('Menu')} className={styles.header__button}>
+      <div className={styles.header__logo}>
         <img src={logo} alt="Logo" />
+      </div>
+      <div className={styles.header__title}>
+        <img src={title} alt="title" />
+      </div>
+      <div onClick={() => alert('Menu')} className={styles.header__button}>
+        <img src={button} alt="Logo" />
       </div>
     </div>
   );
