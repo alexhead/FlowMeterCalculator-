3653e5866838565333a8e00c92e47fa428444d10
Urzael59 <andry137.eremeev@gmail.com>
2024-10-28 14:13:17 +0500

    feat: base position styles for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import * as styles from './Header.module.css';
+import logo from './../../public/images/menuButton.png';
 
 const Header = () => {
-  return <div className={styles.header}>Flow Meter Calculator</div>;
+  return (
+    <div className={styles.header}>
+      <div className={styles.header__logo}>Logo</div>
+      <div className={styles.header__title}>FlowMeterCalc</div>
+      <div onClick={() => alert('Menu')} className={styles.header__button}>
+        <img src={logo} alt="Logo" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
